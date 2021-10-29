7e1ecb587f990decdb43ddcd82d974aae6210934
depth81 <petv1981@gmail.com>
2021-10-29 08:45:09 -0500

    update

--- flask/crudmysql/configdb.py
+++
@@ -1,7 +0,0 @@
-#libreria para conexión a MySQL
-import pymysql
-
-#Método pra realizar la conexión a mysql
-def get_connection():
-    return pymysql.connect(host='localhost',user='root',password='',db='dbbibliotk')
-
--- flask/practicaUno/static/js/layout.js
+++
@@ -1,1 +0,0 @@
-/* alert("Ha ingresado al sistema."); */
