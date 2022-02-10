f0e475e97ce0aea3b550e2444ff23f2cbea7607e
depth81 <petv1981@gmail.com>
2022-02-09 23:20:27 -0500

    zelda update
    https://www.youtube.com/watch?v=QU1pPzEGrqw&t=1s
    timestamp: 1:47:50

--- games/zelda/code/level.py
+++ games/zelda/code/level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
 	def __init__(self):
@@ -18,14 +20,37 @@ class Level:
 		self.create_map()
 
 	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
+		
+		layout = {
+			'boundary':import_csv_layout('../map/map_FloorBlocks.csv'),
+			'grass' : import_csv_layout('../map/map_Grass.csv'),
+			'object': import_csv_layout('../map/map_LargeObjects.csv')
+		}
+		graphics = {
+			'grass' : import_folder('../graphics/grass')	
+		}
+		print(graphics)
+		
+		for style,layout in layout.items():
+			for row_index,row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						if style == 'grass':
+							#create a grass tile
+							random_grass_image = choice(graphics['grass'])
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
+						if style == 'object':
+							#create an object tile
+							pass
+				""" if col == 'x':
 					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
 				if col == 'p':
-					self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+					self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites) """
+		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites) 
 
 	def run(self):
 		# update and draw the game
