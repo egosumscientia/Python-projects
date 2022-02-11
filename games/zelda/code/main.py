22b61f49167ff7a7a5fa219783d25649b63de6b8
depth81 <petv1981@gmail.com>
2022-02-11 15:19:11 -0500

    update

--- games/zelda/code/main.py
+++ games/zelda/code/main.py
@@ -11,34 +11,35 @@ Discord - https://discord.com/invite/a5C6pYw2w5
 
 https://www.youtube.com/watch?v=QU1pPzEGrqw&t=1s '''
 
-
 import pygame, sys
 from settings import *
 from level import Level
 
+
 class Game:
-	def __init__(self):
+    def __init__(self):
 
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
-		self.clock = pygame.time.Clock()
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('Zelda')
+        self.clock = pygame.time.Clock()
 
-		self.level = Level()
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
+        self.level = Level()
 
-			self.screen.fill('black')
-			self.level.run()
-			pygame.display.update()
-			self.clock.tick(FPS)
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
