ae0cdfdffd00a58a84b79d305b92441e7125385b
Paulo Toro <egosumscientia@gmail.com>
2025-03-14 20:30:25 -0500

    avances

---
+++ Portfolio/flask_api_project/app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    @app.route('/')
+    def home():
+        return "¡Flask con migraciones funcionando!"
+
+    return app
